27d224c6e520381d2396b5e28c43daad0c4b6e50
Java7755 <javier.cristani@gmail.com>
2024-05-30 13:59:44 -0300

    subiendo cambios

--- src/routes/shopRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const shopControllers = require("../controllers/shopControllers");
-router.get("/shop", shopControllers.shopView)
-
-module.exports = router;
